a123a9f85b9c83186f75328af1227d975ab81be1
Pierce <102917054+pierceconwi@users.noreply.github.com>
2022-09-07 23:45:59 +0000

    re-commit

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
-
+ 
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
